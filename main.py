2f189fd2965837b3ae3b9a29a204e0b23c121e03
SuiYuan-l <68311553+SuiYuan-l@users.noreply.github.com>
2024-01-02 17:53:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if weather is None:
 data = {
   "city": {
     "value": city,
-    "color": "#173177"
+    "color": get_random_color()
   },
   "date": {
     "value": today.strftime('%Y年%m月%d日'),
